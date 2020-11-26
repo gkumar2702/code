004c4fd0d891cf34795936a9fc5551db0aeac32a
Aman Aggarwal <musigma.aaggarwal.c@aes.com>
2020-11-26 21:46:52 +0530

    Changes

--- Development/storm_level_comparison_pylint.py
+++ Development/storm_level_comparison_pylint.py
@@ -1,32 +1,27 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 """
-DAG for diagnostic view dashboard
+Authored: Mu Sigma
+Updated: 26 Nov 2020
+Version: 1.5
+Tasks: Compute actuals no of outages and customers affected
+and aggregate to day level to compare with predictions
 """
 
+# standard library imports 
 import warnings
 import logging
 from datetime import datetime
 import pandas as pd
 import numpy as np
 from pandas.io import gbq
-from google.cloud import storage
 import pandas_gbq as gbq
 
+# third party improts
+from google.cloud import storage
 
-warnings.filterwarnings('ignore')
-pd.options.mode.chained_assignment = None  # default='warn'
-
-pd.set_option('display.max_rows', 1000)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 2000)
-pd.options.display.float_format = '{:.2f}'.format
-
+# setup logs
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
 CURRENT_DATE = datetime.today().strftime('%Y-%m-%d')
-print(CURRENT_DATE)
 CLIENT = storage.Client()
 BUCKET_NAME = 'aes-datahub-0001-raw'
 BUCKET = CLIENT.get_bucket(BUCKET_NAME)
@@ -34,84 +29,55 @@ BUCKET = CLIENT.get_bucket(BUCKET_NAME)
 BLOBS = BUCKET.list_blobs(prefix='OMS/'+CURRENT_DATE)
 DIRLIST = []
 
-
 for blob in BLOBS:
-#     print(blob.name)
     DIRLIST.append(str(blob.name))
 
 _MATCHING_FACILITY = [s for s in DIRLIST if "FACILITY_IPL_Daily" in s]
 _MATCHING_LIVE_FACILITY = [s for s in _MATCHING_FACILITY if "HIS" in s]
-print(_MATCHING_LIVE_FACILITY)
-print('\n')
 
 BUCKET_NAME = 'gs://aes-datahub-0001-raw/'
 
-print(CURRENT_DATE)
-print('\n')
-logging.info('%s', CURRENT_DATE)
-logging.info('\n')
+logging.info('Todays Date %s \n', CURRENT_DATE)
 
 FACILITY = pd.read_csv(BUCKET_NAME + _MATCHING_LIVE_FACILITY[0], encoding="ISO-8859-1", sep=",")
 
-print(FACILITY.shape)
-
 ## QC checks
-print("****QC Check****")
-print("Shape of HIS Incident Device Table")
-print(FACILITY.shape)
-print("\n")
-print("****QC Check****")
-print("No of Distinct INCIDENT_ID in INCIDENT_DEVICE Table")
-print(FACILITY.FAC_JOB_ID.nunique())
-print("\n")
+logging.info('****QC Check****')
+logging.info('Shape of HIS Incident Device Table %s \n', FACILITY.shape)
+logging.info('****QC Check****')
+logging.info('No of Distinct INCIDENT_ID in INCIDENT_DEVICE Table %s \n', FACILITY.FAC_JOB_ID.nunique())
 
-
-# # **Apply Required Filters**
-
-####### APPLYING FILTERS FOR CORRECT DATA INPUTS ###########
-
+# **Apply Required Filters**
+# APPLYING FILTERS FOR CORRECT DATA INPUTS ###########
 # customer quantity greater than 0
-print('Filter for customer quantity greater than 0')
-# print("****QC Check****")
-print("Rows left after checking for INCIDENTS whose CUSTOMER QUANTITY IS > 0")
+logging.info('Filter for customer quantity greater than 0')
 FACILITY = FACILITY[(FACILITY.DOWNSTREAM_CUST_QTY > 0)]
-print(FACILITY.shape)
-print("\n")
+logging.info("Rows left after checking for INCIDENTS whose CUSTOMER QUANTITY IS > 0 %s \n", FACILITY.shape)
 
 # equip_stn_no is not NCC and not null
-print('Filter for equp_stn_no is not NCC or not null')
-# print("****QC Check****")
-print("Rows left after checking that EQUIP_STN_NO is not from <<NON CONNECTED CUSTOMERS>>")
+logging.info('Filter for equp_stn_no is not NCC or not null')
+# logging.info("****QC Check****")
+logging.info("Rows left after checking that EQUIP_STN_NO is not from <<NON CONNECTED CUSTOMERS>> %s \n", FACILITY.shape)
 FACILITY = FACILITY[(FACILITY.EQUIP_STN_NO != '<NCC>') & (FACILITY.EQUIP_STN_NO.notnull())]
-print(FACILITY.shape)
-print("\n")
 
 # removing NAN from DNI_EQUIP_TYPE, CIRCT_ID, STRCTUR_NO
-print('Removing NAN from DNI_EQIP_TYPE, CICRT_ID, STRCTUR_NO')
-# print("****QC Check****")
-print("Rows left after checking CIRCT_ID is not\
-    0 and not null STRCTUR_NO is not null and DNI_EQIP_TYPE is not null")
+logging.info('Removing NAN from DNI_EQIP_TYPE, CICRT_ID, STRCTUR_NO')
 FACILITY = FACILITY[(FACILITY.CIRCT_ID != 0)]
 FACILITY = FACILITY[~FACILITY.CIRCT_ID.isnull()]
 FACILITY = FACILITY[~FACILITY.STRCTUR_NO.isnull()]
 FACILITY = FACILITY[~FACILITY.DNI_EQUIP_TYPE.isnull()]
-print(FACILITY.shape)
-print("\n")
+logging.info("Rows left after checking CIRCT_ID is not\
+    0 and not null STRCTUR_NO is not null and DNI_EQIP_TYPE is not null %s \n", FACILITY.shape)
 
 # removing CLUE_CD which start with 0 but does not start with 00
-print('Removing CLUE_CD which start with 0 but do not start with 00')
-# print("****QC Check****")
-print("Rows left after filtering for CLUE CODES which start with 0 but do not start with 00")
+logging.info('Removing CLUE_CD which start with 0 but do not start with 00')
 FACILITY = FACILITY[(FACILITY.CLUE_CD.str[:1] == '0') & (FACILITY.CLUE_CD.str[:2] != '00')]
 FACILITY = FACILITY[FACILITY.CLUE_CD != '01']
-print(FACILITY.shape)
-print("\n")
+FACILITY = FACILITY[FACILITY.CLUE_CD != '09OD']
+logging.info("Rows left after filtering for CLUE CODES which start with 0 but do not start with 00 %s \n", FACILITY.shape)
 
 # removing occurence codes starting with cancel, found ok and duplicate
-print('Removing CLUE_CD which start with 0 but do not start with 00')
-# print("****QC Check****")
-print("Rows left after removing OCCURN_CD which have\
-    descriptions starting with CANCEL, FOUND OK or DUPLICATE")
+logging.info('Removing CLUE_CD which start with 0 but do not start with 00')
 OCCUR_REMOV = [30003001, 33003301, 33003302, 34003400, 34003401, 34003402, 34003403,
                34003404, 34003405, 34003406, 34003407, 34003408, 34003409, 35003500,
                35003501, 35003502, 35003503, 35003504, 35003505, 35003506, 35003507,
@@ -127,22 +93,19 @@ OCCUR_REMOV = [30003001, 33003301, 33003302, 34003400, 34003401, 34003402, 34003
                36503658]
 
 FACILITY = FACILITY[~(FACILITY.OCCURN_CD.isin(OCCUR_REMOV))]
-print(FACILITY.shape)
-print("\n")
+logging.info("Rows left after removing OCCURN_CD which have\
+    descriptions starting with CANCEL, FOUND OK or DUPLICATE %s \n", FACILITY.shape)
 
-DF_NUMERICAL = FACILITY
+DF_NUMERICAL = FACILITY.copy(deep=True)
 
-
-# # **Aggregate Numerical Columns**
-
+# **Aggregate Numerical Columns**
 '''
 Create outage id columns
 remove and filter outages only after the trigger
 '''
 
 ## START ADS CREATION FOR NUMERICAL COLUMNS AT INCIDENT LEVEL
-
-# 1.1 Aggregate numerical columns at INCIDENT_ID level to keep all unique INCIDNET_ID's
+# 1.1 Aggregate numerical columns at INCIDENT_ID level to keep all unique 'INCIDENT_ID', 'STRCTUR_NO', 'CIRCT_ID', 'DNI_EQUIP_TYPE'
 DF_NUMERICAL = FACILITY.groupby(['INCIDENT_ID', 'STRCTUR_NO', 'CIRCT_ID', 'DNI_EQUIP_TYPE'],
                                 as_index=False).agg({'CALL_QTY':'sum',
                                                      'DOWNSTREAM_CUST_QTY':'sum',
@@ -164,19 +127,15 @@ DF_NUMERICAL['OUTAGE_ID'] = DF_NUMERICAL.apply(lambda x: '%s%s%s%s' % (x['INCIDE
                                                                        x['CIRCT_ID'],
                                                                        x['DNI_EQUIP_TYPE']), axis=1)
 
-print("****QC Check****")
-print("Shape of Numerical columns at 'INCIDENT_ID','STRCTUR_NO','CIRCT_ID','DNI_EQUIP_TYPE' Level")
-print(DF_NUMERICAL.shape)
-print('\n')
+logging.info("****QC Check****")
+logging.info("Shape of Numerical columns at 'INCIDENT_ID','STRCTUR_NO','CIRCT_ID','DNI_EQUIP_TYPE' Level %s \n", DF_NUMERICAL.shape)
 
 DF_NUMERICAL['CREATION_DATETIME'] = pd.to_datetime(DF_NUMERICAL['CREATION_DATETIME'])
 DF_NUMERICAL['ENERGIZED_DATETIME'] = pd.to_datetime(DF_NUMERICAL['ENERGIZED_DATETIME'])
-DF_NUMERICAL['TTR'] = (DF_NUMERICAL['ENERGIZED_DATETIME'] - DF_NUMERICAL['CREATION_DATETIME']).astype('timedelta64[s]')
-DF_NUMERICAL['TTR'] = DF_NUMERICAL['TTR']/60
-
+DF_NUMERICAL['TTR'] = (DF_NUMERICAL.ENERGIZED_DATETIME - DF_NUMERICAL.CREATION_DATETIME).dt.total_seconds().div(60).round(4)
 DF_NUMERICAL['TTR'] = DF_NUMERICAL['TTR'].round(decimals=0)
 
-DF_NUMERICAL['TTR'] = DF_NUMERICAL[DF_NUMERICAL['TTR']>5]
+DF_NUMERICAL = DF_NUMERICAL[DF_NUMERICAL['TTR']>30]
 
 DF_NUMERICAL['Date'] = DF_NUMERICAL.CREATION_DATETIME.dt.date
 
